b9070ab545efc9980ea81eca9d53dea1061cdeb2
Gopinath WorkSpace <gbnathworkspace@gmail.com>
2025-09-24 07:56:13 +0530

    - added CRUD top lvl controllers for productService

--- product-service/src/main/java/com/example/products/controller/ProductController.java
+++ product-service/src/main/java/com/example/products/controller/ProductController.java
@@ -1,17 +1,43 @@
 package com.example.products.controller;
 
+import com.example.products.dto.request.ProductRequest;
 import com.example.products.dto.response.ProductResponse;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
     @GetMapping("/get")
-    public ResponseEntity<ProductResponse> GetAllProducts()
+    public ResponseEntity<List<ProductResponse>> GetAllProducts()
+    {
+        return ResponseEntity.ok(new ArrayList<ProductResponse>(){});
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<ProductResponse> GetProducts(@RequestParam(name="user")String userId)
+    {
+        return ResponseEntity.ok(new ProductResponse());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ProductResponse> AddProduct(@RequestBody ProductRequest productRequest)
+    {
+        return ResponseEntity.ok(new ProductResponse());
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<ProductResponse> DeleteProduct(@RequestParam(name = "pid")String productId)
+    {
+        return ResponseEntity.ok(new ProductResponse());
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<ProductResponse> UpdateProduct(@RequestBody ProductRequest productRequest, String productId)
     {
         return ResponseEntity.ok(new ProductResponse());
     }
---
+++ product-service/src/main/java/com/example/products/dto/request/ProductRequest.java
@@ -0,0 +1,10 @@
+package com.example.products.dto.request;
+
+import java.time.LocalDateTime;
+
+public class ProductRequest {
+    public String name;
+    public String price;
+    public String description;
+    private String category;
+}
